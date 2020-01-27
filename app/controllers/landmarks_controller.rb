defd23e6466931425ef576a1f11a20bb91723f37
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2020-01-27 17:15:04 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,7 +17,7 @@ class LandmarksController < ApplicationController
     erb :'landmarks/show'
   end
 
-  get '/landmarks/:id/edit'
+  get '/landmarks/:id/edit' do
   @landmark = Landmark.find(params[:id])
 
   erb :'landmarks/edit'
