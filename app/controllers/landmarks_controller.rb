5a9bca277bd045458f4f9d334c3b85f151693873
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2020-01-27 17:05:12 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,6 +11,15 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
   post '/landmarks' do
     @landmark = Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
     redirect '/landmarks'
