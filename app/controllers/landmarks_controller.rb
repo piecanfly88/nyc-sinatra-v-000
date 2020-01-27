2cac0f95abf549d5d9ff688c2f97021c73b28f8a
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2020-01-27 17:14:08 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -34,7 +34,7 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     @landmark = Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
-    redirect to '/landmarks'
+    redirect '/landmarks'
   end
 
 end
